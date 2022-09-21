c3078b72760b61bb50fd02546bcabe7bb5f13926
Arq. Software <20393160@utcancun.edu.mx>
2022-09-20 22:11:27 -0500

    api

---
+++ src/app/2Api/nombres.service.ts
@@ -0,0 +1,17 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class NombresService {
+//   constructor(public _http: HttpClient) {
+
+//   }
+
+//   getdata<T> (url : string)
+//   {
+//    url = 'http://universities.hipolabs.com/search?country=United+States'
+//    return this._http.get<T[]>(url);
+//   }
+// }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
+import { UsuariosService } from '../ApiBuena/usuarios.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+getdata:any []=[];
+  constructor( public _services: UsuariosService) {
+    this._services.getdata<any[]>("").subscribe(data=>{
+      this.getdata = data
+      console.log(this.getdata);
+    }
+      )
+  }
 
 }
